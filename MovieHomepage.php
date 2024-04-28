59cff3773384df6f2c6fc2ab4e3ccf1bc785bfd7
Bruce Metoyer <150362748+BruceMetoyer@users.noreply.github.com>
2024-04-28 14:35:59 -0400

    MovieHomepage.php

---
+++ MovieHomepage.php
@@ -0,0 +1,301 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Bruce Metoyer, Udoka Ndolo, Gregory Walters, Kevin Eliasmejia">
+    <meta name="description" content="This page contains multiple 
+    websites along with information from tables such as
+    actors, diretors, genre, and movie.">
+    <title>BUGK Films</title>
+    <link rel="icon" href="images/movie-logo-vector-19783450.jpg" type="image/x-icon">
+    <link rel=" stylesheet" href="main.css" type="text/css">
+</head>
+
+
+<style>
+    .poster {
+        box-shadow: 0 0 15px blueviolet !important;
+
+    }
+
+    .buddy {
+        cursor: pointer;
+    }
+
+    #scrollToTopButton {
+        position: fixed;
+        bottom: 40px;
+        right: 25px;
+        font-size: 25px;
+        z-index: 99;
+        width: 50px;
+        height: 50px;
+        background-color: blueviolet;
+        color: black;
+        border: none;
+        cursor: pointer;
+        outline: none;
+        padding: 6px;
+        border-radius: 50%;
+    }
+
+    #scrollToTopButton:hover,
+    i:hover {
+        background-color: white;
+        color: blueviolet;
+    }
+
+    .navbar-nav {
+        display: flex;
+        align-items: center;
+        padding: 0px 7.5px;
+    }
+
+
+
+
+    .maincontainer h3 {
+        color: black;
+        text-align: center;
+    }
+
+    .container {
+        text-align: center;
+    }
+
+    #navbarNav.nav-item.nav-link a:hover {
+        color: blueviolet;
+    }
+
+    .nav-link:hover {
+        color: blueviolet;
+    }
+
+    .nav-item :hover {
+        margin-bottom: 10px;
+
+
+    }
+
+    #header-nav .navbar-nav .nav-item .nav-link:hover {
+        color: blueviolet;
+        text-decoration: none;
+        /* Remove underline */
+        transition: color 0.3s ease, text-shadow 0.3s ease;
+        /* Add transition effect */
+        text-shadow: 0 0 10px blueviolet;
+        /* Add glowing effect */
+    }
+</style>
+
+<body>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="MovieHomepage.php"><img class="logo" src="images/movie-logo-vector-19783450.jpg" alt="" width="54" height="34"></a>
+        <h1>Welcome to BUGK Films!</h1>
+        <marquee>Welcome To <i>BUGK Films</i>.com<sup>&reg;</sup></marquee>
+        <label class="online">Stream Some of your favorite Movies Today!</label>
+        <hr>
+        <header>
+
+            <p>BUGK Films contains a large selection of some of your favorite movies! This selection of
+                popular films provide movie titles and ratings along with crucial information from a database such as
+                the actors in the movies, diretor of each movie and the genre of each movie.</p>
+            <hr>
+            <h2>Genres</h2>
+            <ul>
+                <li>
+                    <a href="#Action">Action Films</a>
+                </li>
+                <li>
+                    <a href="#Comedy">Comedy Films</a>
+                </li>
+                <li>
+                    <a href="#Science_Fiction">Science Fiction Films</a>
+                </li>
+                <li>
+                    <a href="#Romace">Romance Films</a>
+                </li>
+                <li>
+                    <a href="#Horror">Horror Films</a>
+                </li>
+                <li>
+                    <a href="#Thriller">Thriller Films</a>
+                </li>
+                <li>
+                    <a href="#Drama">Drama Films</a>
+                </li>
+                <li>
+                    <a href="#Adventure">Adventure Films</a>
+                </li>
+                <li>
+                    <a href="#Mystery">Mystery Films</a>
+                </li>
+                <li>
+                    <a href="#Sports">Science Fiction Films</a>
+                </li>
+            </ul>
+            <hr>
+
+            <div class="buddy">
+
+                <div class="slider">
+                    <div class="slide_track">
+
+                        <div class="slide">
+                            <img src="images/Dune2.jpg" alt="" />
+                        </div>
+
+                        <div class="slide">
+                            <img src="images/kung_fu_panda_4.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/kong_godzilla.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/interstellar.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/civil_war.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/monkey_man.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/friday.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/the_notebook.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/the_conjuring.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/a_walk_to_remember.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/split.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/the_godfather.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/the_mummy.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/morder_on_the_orient_express.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/remember_the_titans.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/catwoman.jpg" alt="" />
+                        </div>
+
+
+                        <div class="slide">
+                            <img src="images/Dune2.jpg" alt="" />
+                        </div>
+
+                        <div class="slide">
+                            <img src="images/kung_fu_panda_4.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/kong_godzilla.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/interstellar.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/civil_war.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/monkey_man.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/friday.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/the_notebook.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/the_conjuring.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/a_walk_to_remember.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/split.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/the_godfather.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/the_mummy.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/morder_on_the_orient_express.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/remember_the_titans.jpg" alt="" />
+                        </div>
+                        <div class="slide">
+                            <img src="images/catwoman.jpg" alt="" />
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+            </div>
+
+            <div class="maincontainer">
+
+                <h2>Movies</h2>
+                <hr>
+                <div class="container">
+                    <div id="movies" class="row"></div>
+                </div>
+                <div class="container" id="Movies">
+                    <h3>Action</h3>
+                    <hr>
+                    <div class="poster">
+                        <div class="flip-card_i">
+                            <div class="flip-card-inner_i">
+                                <div class="flip-card-front_i">
+                                    <img src="images/Dune2.jpg" alt="Avatar" style="width:250px;height:360px;">
+                                </div>
+                                <div class="flip-card-back_i">
+                                    <h1>Dune: Part Two</h1>
+                                    <button class="btn_i b3_i" title="Year" style="color:white">2010</button>
+                                    <button class="btn_i b2_i" title="Minutes" style="color:white">2h42m<button>
+                                            <button class="btn_i b1_i" title="IMDb" style="color:white">8.8</button>
+                                            </span>
+                                            <br />
+                                            <br />
+                                            </p>
+
+                                            <p class="para_i">Paul Atreides unites with Chani and the Fremen while seeking revenge against the conspirators who destroyed his family.
+                                                Facing a choice between the love of his life and the fate of the universe, he must prevent a terrible future only he can foresee.</p>
+
+                                            <button class="trailer_i" type="submit" value="" onclick="openWin()">▶Trailer</button>
+                                            <script type="text/javascript">
+                                                function openWin() {
+                                                    window.open("https://youtu.be/Way9Dexny3w?si=aKknZr31cL0Kh5CX", "_blank", "top=100,left=250,height=400,width=600,channelmode=yes,fullscreen=yes,menubar=no,toolbar=no,location=no,status=no,scrollbars=no,noopener=no");
+                                                }
+                                            </script>
+                                </div>
+                            </div>
+                            <div class="flim">
+                                <b>Dune:Par Two</b><br>IMDb - 8.8
+                            </div>
+                        </div>
+                    </div>
+
+                    <h2>Movie Database Information</h2>
+                    <?php include 'movieconnection.php'; ?>
+                    <p> HElooooooooo</p>
+</body>
+
+</html>
